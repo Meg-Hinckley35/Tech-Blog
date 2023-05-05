9d9730f74e73d37267b42d774c2d159201b415d9
MegMcC <116989994+Meg-Hinckley35@users.noreply.github.com>
2023-05-05 16:56:47 -0400

    set get route for editing an existing post, console log error if not found

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -46,3 +46,47 @@ router.get('/', withAuth, (req, res) => {
         res.status(500).json(err);
       });
   });
+
+  // get route to edit a post
+router.get('/edit/:id', withAuth, (req, res) => {
+    // single post is obtained from the database
+    Post.findOne({
+      where: {
+        id: req.params.id
+      },
+      attributes: [
+        'id',
+        'post_text',
+        'title',
+        'created_at',
+      ],
+      include: [
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    })
+    .then(dbPostData => {
+        // if no post by that id exists, return an error
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+        // serialize data before passing to template
+        const post = dbPostData.get({ plain: true });
+        res.render('edit-post', { post, loggedIn: true });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
