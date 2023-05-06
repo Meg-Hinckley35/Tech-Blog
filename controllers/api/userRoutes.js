a0f8c88fa62d193c1451ea7fca9f1ed7798a1249
MegMcC <116989994+Meg-Hinckley35@users.noreply.github.com>
2023-05-06 17:41:11 -0400

    set route to get all users

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,11 +1,29 @@
-// Dependencies
-// Express.js connection
+// dependencies
+// express.js connection
 const router = require('express').Router();
-// User, Post, Vote models
+// User, Post, Comment models
 const { User, Post, Comment } = require('../../models');
-// Express Session for the session data
+// express Session for the session data
 const session = require('express-session');
-// Authorization Helper
+// authorization Helper
 const withAuth = require('../../utils/auth');
-// Sequelize store to save the session so the user can remain logged in
+// sequelize store to save the session so the user can remain logged in
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+// routes
+
+// GET /api/users -- get all users
+router.get('/', (req, res) => {
+    // access the User model and run .findAll() method to get all users
+    User.findAll({
+        // when data is sent back, exclude password
+        attributes: { exclude: ['password'] }
+    })
+      // return the data as JSON formatted
+      .then(dbUserData => res.json(dbUserData))
+      // if there is a server error, return that error
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
