411f59ff8c6de9c70681731b807fb2fe70596da8
MegMcC <116989994+Meg-Hinckley35@users.noreply.github.com>
2023-05-06 17:45:38 -0400

    set post route to add new user

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -69,3 +69,27 @@ router.get('/:id', (req, res) => {
         res.status(500).json(err);
       });
   });
+
+// POST /api/users -- add a new user
+router.post('/', (req, res) => {
+    User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password
+    })
+      // send user data back to the client as confirmation and save the session
+      .then(dbUserData => {
+        req.session.save(() => {
+          req.session.user_id = dbUserData.id;
+          req.session.username = dbUserData.username;
+          req.session.loggedIn = true;
+      
+          res.json(dbUserData);
+        });
+      })
+      // if there is a server error, return that error
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });  
