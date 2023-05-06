8e94706deca8cb69c9ab09dbeda7c2e14cabc241
MegMcC <116989994+Meg-Hinckley35@users.noreply.github.com>
2023-05-06 17:55:44 -0400

    set get route to get all comments

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -4,3 +4,17 @@ const { Comment } = require('../../models');
 // Authorization Helper
 const withAuth = require('../../utils/auth');
 
+// routes
+
+// GET comments
+router.get('/', (req, res) => {
+    // access Comment model and run .findAll() to get all comments
+    Comment.findAll()
+      // return the data as JSON
+      .then(dbCommentData => res.json(dbCommentData))
+      // if there is a server error, return that error
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
