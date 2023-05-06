5b665c59acececbc721b1681501d7b59e1dbfe51
MegMcC <116989994+Meg-Hinckley35@users.noreply.github.com>
2023-05-06 18:37:03 -0400

    putting in the model associations to define the relationships between each

--- models/index.js
+++ models/index.js
@@ -4,3 +4,27 @@ const User = require('./User');
 const Post = require('./Post');
 // Comment model
 const Comment = require('./Comment');
+
+// define associations between the models
+// User-Post relationship
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+//Post-User relationship
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+// Comment-User relationship
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'cascade',
+    hooks:true
+});
+
+// Comment-Post relationship
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id',
+    onDelete: 'cascade',
+    hooks: true
+});
