7c83fd2f60ba799d9409c6b42076c54c01861b03
MegMcC <116989994+Meg-Hinckley35@users.noreply.github.com>
2023-05-06 18:01:44 -0400

    added post routes file and set dependencies

---
+++ controllers/api/postRoutes.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+// User Model, Post Model, and Comment Model
+const { User, Post, Comment } = require('../../models');
+// Sequelize database connection
+const sequelize = require('../../config/connection');
+// Authorization Helper
+const withAuth = require('../../utils/auth');
