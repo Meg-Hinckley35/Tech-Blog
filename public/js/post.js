4c392c291d88b3edbd39a5b1fe586e3105744d82
MegMcC <116989994+Meg-Hinckley35@users.noreply.github.com>
2023-05-06 21:15:14 -0400

    added edit post form handler

--- public/js/post.js
+++ public/js/post.js
@@ -25,6 +25,41 @@ async function newFormHandler(event) {
       alert(response.statusText);
     }
   }
+
+  // Edit a post for handler
+async function editFormHandler(event) {
+    event.preventDefault();
+
+    // get the post id from the url
+    const id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+    ];
+
+    // Get the post title and post text from the form
+    const title = document.querySelector('input[name="post-title"]').value;
+    const post_text = document.querySelector('textarea[name="post-text"]').value;
+
+    // use the update route to update the post
+    const response = await fetch(`/api/posts/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify({
+          title,
+          post_text
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+
+    if (response.ok) {
+        document.location.replace('/dashboard');
+        } else {
+        alert(response.statusText);
+        }
+
+  }
   
-  // event listener for the new post submit button
+  document.querySelector('.edit-post-form').addEventListener('submit', editFormHandler);
+  
+  // event listeners
   document.querySelector('.new-post-form').addEventListener('submit', newFormHandler);
